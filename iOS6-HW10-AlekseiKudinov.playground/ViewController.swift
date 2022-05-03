75cfb93be7cc3fb8ec514077bbe63fd7ff3afafa
AlekseiKudinov <machette94@gmail.com>
2022-05-03 14:05:49 +0300

    Initial Commit

---
+++ iOS6-HW10-AlekseiKudinov.playground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW10-AlekseiKudinov.playground
+//
+//  Created by Алексей Кудинов on 03.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
